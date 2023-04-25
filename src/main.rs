aff84c20d130df1590c7fdd74b88f55848c19b87
Emily <emilyeodonovan@gmail.com>
2023-04-25 22:20:25 +0100

    buffered file reader

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,44 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
+
 fn main() {
     println!("Hello, {}!", "rusty");
-    binary_interpretation();
-}
-
-fn binary_interpretation() {
-    //read file line by line
-    //extract relevant data
+    //Place each of the below into seperate rust modules (files)
+    //generate reference points on first run
+    let star_at = binary_interpretation();
+    //genereates star_triples on second, file read can be excluded
+    //generates misc star data on third
     
-    //per star generate reference points, and store in a vector
-    //use galactic coordinates and geometry to relate stars to each other within an arbitrary angle
-    //store the angular relationship between groups of 3 stars along their bright_star_number
-
-
-    //format for firebase
 }
 
-//  fn new_star(bytes: Vec<u8>) -> Star {
+fn binary_interpretation() -> Vec<StarAt> {
+    let star_at: Vec<StarAt> = Vec::new();
+    //if result is Ok, opens the file and puts it into buffer "lines"
+    if let Ok(lines) = read_lines("asuNoHeader.tsv") {
+        // consumes lines and iterates over each line
+        for line in lines {
+            //if line sucessfully covertts to string it is consumed
+            if let Ok(ip) = line {
+                println!("{}", ip);
+                
+            }
+        }
+    }
+    star_at
+}
 
-// }
+
+//// Returns an Iterator wrapped in a result to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+
 
 struct Star {
     pub bright_star_num: u32, 
@@ -34,6 +55,7 @@ struct StarAt {
     pub galactic_long: u64, //galactic longitude 5 bytes
     pub galactic_lat: u64 //galactic latitude
 }
+
 
 struct star_triple {
     pub bsm_1: u32, //bright star number 1
